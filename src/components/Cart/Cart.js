94f0fecbfe6c40bf2a427beaed859839212d70de
Ivan Zarkovic <ivan.zarkovic@ewe.rs>
2022-04-27 14:56:09 +0200

    handle removing items from cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,28 +5,33 @@ import CartContext from '../../store/cart-state';
 import CartItem from './CartItem'
 
 const Cart = (props) => {
-
     const cartContext = useContext(CartContext);
-
+  
     const totalAmount = ` RSD ${cartContext.totalAmount.toFixed(2)}`;
     const hasItems = cartContext.items.length > 0;
-
-    const cartItemRemoveHandler = id => {};
-    const cartItemAddhandler = item => {};
-
+  
+    const cartItemRemoveHandler = (id) => {
+      cartContext.removeItem(id);
+    };
+  
+    const cartItemAddHandler = (item) => {
+      cartContext.addItem({...item, amount: 1});
+    };
+  
     const cartItems = (
-        <ul className={classes['cart-items']}>
-            {cartContext.items.map((item) => (
-                <CartItem 
-                key={item.id} 
-                name={item.name} 
-                amount={item.amount} 
-                price={item.price} 
-                onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                onAdd={cartItemAddhandler.bind(null, item)}
-                />
-            ))}
-        </ul>);
+      <ul className={classes['cart-items']}>
+        {cartContext.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        ))}
+      </ul>
+    );
 
       return (
         <Modal onClose={props.onClose}>
